76225030484401f6854ce84205118ce7f85de7d0
BoDee <nyx0hemir@gmail.com>
2021-05-20 17:36:17 -0600

    finished editing feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,31 @@
 var tasks = {};
 
 var createTask = function(taskText, taskDate, taskList) {
-  // create elements that make up a task item
+  // create elements that make up an individual task.
   var taskLi = $("<li>").addClass("list-group-item");
+  // -creates a <li> DOM element and assigns it the class 'list-group-item
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
+  // -creates a <span> DOM element and assigns it a class, as well as text-
+  // that will be given as a parameter later.
   var taskP = $("<p>")
     .addClass("m-1")
     .text(taskText);
+  // -creates a <p> element and assigns it a class and text that will be-
+  // given as a parameter later in the code.
 
-  // append span and p element to parent li
+  // append span and p element to parent li.
   taskLi.append(taskSpan, taskP);
 
-
-  // append to ul list on the page
+  // append to ul list on the page, without this the three DOM elements that-
+  // were just created will not appear on the page, only existing in memory.
   $("#list-" + taskList).append(taskLi);
 };
 
 var loadTasks = function() {
   tasks = JSON.parse(localStorage.getItem("tasks"));
+  // -assigns the tasks object corresponding data stored in local storage, parsed.
 
   // if nothing in localStorage, create a new object to track all task status arrays
   if (!tasks) {
@@ -33,7 +39,6 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -45,8 +50,97 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+    .text()
+    .trim();
+  console.log(text);
+  var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+});
 
+$(".list-group").on("blur", "textarea", function() {
+  // get the textarea's current value/text
+  var text = $(this)
+    .val()
+    .trim();
+  
+  //get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  
+  //get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
 
+    tasks[status][index].text = text;
+    saveTasks();
+
+    //recreate p element
+    var taskP = $("<p>")
+      .addClass("m-1")
+      .text(text);
+    
+    //replace text area with p element
+    $(this).replaceWith(taskP);
+});
+
+//due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function(){
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to local storage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+  
+  //replace input with span element
+  $(this).replaceWith(taskSpan);
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
